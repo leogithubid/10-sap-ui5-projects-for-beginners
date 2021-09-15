277e544208222bb1b288f3b0dd8de52a1a5d6802
leo <40744320+leogithubid@users.noreply.github.com>
2021-09-16 07:52:58 +1000

    flexible column layout - initial
    flexible column layout - initial

---
+++ node_modules/generator-easy-ui5/generators/newview/templates/webapp/controller/$ViewName.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "<%= controllerToExtend%>"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("<%= namespace%>.<%=projectname%>.controller.<%=viewname%>", {});
+});
